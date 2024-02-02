34385dccc2021562b7c433d7e5c7c716227640f0
zakzakst <e1433104@gmail.com>
2024-02-02 18:28:25 +0900

    2-2

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -11,6 +11,12 @@ root.render(
   </React.StrictMode>
 );
 
+// setInterval(() => {
+//   root.render(
+//     <p>現在時刻：{(new Date()).toLocaleString()}</p>
+//   )
+// }, 1000)
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
