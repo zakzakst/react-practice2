32de205cce8c49ca3c80195b1519ce5453da54fb
zakzakst <e1433104@gmail.com>
2024-02-04 23:52:21 +0900

    3-2-1

--- my-react/src/chap03/StateBasic.js
+++ my-react/src/chap03/StateBasic.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 export default function StateBasic({ init }) {
   const [count, setCount] = useState(init)
+  console.log(`count is ${count}`)
   const handleClick = () => setCount(count + 1)
 
   return (
