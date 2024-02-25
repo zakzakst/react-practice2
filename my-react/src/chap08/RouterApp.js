828435217d33e36770e026a8ef53209eb3d99c99
zakzakst <e1433104@gmail.com>
2024-02-25 13:30:59 +0900

    8-2-1

---
+++ my-react/src/chap08/RouterApp.js
@@ -0,0 +1,15 @@
+import { Link, Outlet } from 'react-router-dom'
+
+export default function RouterApp() {
+  return (
+    <>
+      <ul>
+        <li><Link to='/'>トップ</Link></li>
+        <li><Link to='/article'>公開記事</Link></li>
+        <li><Link to='/about'>このサイトについて</Link></li>
+      </ul>
+      <hr />
+      <Outlet />
+    </>
+  )
+}
