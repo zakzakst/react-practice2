74c4aea45bedea777f111c49b27dbe8ac9cfbe40
zakzakst <e1433104@gmail.com>
2024-03-02 19:57:35 +0900

    10-2-5

--- my-react-ts/src/ListTemplate.tsx
+++ my-react-ts/src/ListTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { FC, ReactNode } from 'react'
 import type { Book } from './Book'
 
 type ListTemplateProps = {
@@ -6,7 +6,21 @@ type ListTemplateProps = {
   children: (b: Book) => ReactNode
 }
 
-export default function ListTemplate({ src, children }: ListTemplateProps) {
+// export default function ListTemplate({ src, children }: ListTemplateProps) {
+//   return (
+//     <dl>
+//       {
+//         src.map((el, index) => (
+//           <React.Fragment key={el.isbn}>
+//             {children(el)}
+//           </React.Fragment>
+//         ))
+//       }
+//     </dl>
+//   )
+// }
+
+const ListTemplate: FC<ListTemplateProps> = ({ src, children }) => {
   return (
     <dl>
       {
@@ -19,3 +33,5 @@ export default function ListTemplate({ src, children }: ListTemplateProps) {
     </dl>
   )
 }
+
+export default ListTemplate
