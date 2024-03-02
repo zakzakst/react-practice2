041e490ef62cfd3fd657b48e3a0105dccfbfde21
zakzakst <e1433104@gmail.com>
2024-03-02 19:37:38 +0900

    10-2-3

---
+++ my-react-ts/src/ThemeContext.tsx
@@ -0,0 +1,12 @@
+import { PaletteMode } from '@mui/material'
+import { createContext } from 'react'
+
+export type ThemeContextType = {
+  mode: PaletteMode
+  toggleMode: () => void
+}
+
+export default createContext<Partial<ThemeContextType>>({
+  mode: 'light',
+  toggleMode: () => {}
+})
